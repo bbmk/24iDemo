be1009ab695a31e405a6805de7708d795b7617d6
Bojan Angjeleski <bojan.angjeleski@musala.com>
2022-03-29 10:11:14 +0200

    Implement networking

--- 24iTest/Network/NetworkRouter.swift
+++ 24iTest/Network/NetworkRouter.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+
+enum NetworkRouter {
+    case getPopularMoviesList
+    case getMovieDetails(movieId: String)
+    
+    var accessToken: String {
+        switch self {
+        case .getPopularMoviesList, .getMovieDetails:
+            return "03a9dfd3c42397a31ca7ef7c0d4529fb"
+        }
+    }
+    
+    var scheme: String {
+        return "https"
+        // it can be extended
+//        switch self {
+//        case .getPopularMoviesList, .getMovieDetails:
+//            return "https"
+//        default:
+//            return "http"
+//        }
+    }
+    
+    var host: String {
+        // It can be easy adapted for multiple hosts
+        let baseSport = "api.themoviedb.org"
+        switch self {
+        case .getPopularMoviesList, .getMovieDetails:
+            return baseSport
+        }
+    }
+    
+    var port: Int? {
+        return nil
+    }
+    
+    var stringParameters: String {
+        switch self {
+        case .getMovieDetails(let movieId):
+            return movieId
+        case .getPopularMoviesList:
+            return ""
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        return [URLQueryItem(name: "api_key", value: accessToken)]
+        // Can be modified for multiple accessKeys/ tokens
+//        switch self {
+//        case .getPopularMoviesList, .getMovieDetails:
+//            return [URLQueryItem(name: "api_key", value: accessToken)]
+//        default :
+//            return []
+//
+//        }
+    }
+    
+    //    var httpBody: Data? {
+    //
+    //    }
+    
+    var path: String {
+        switch self {
+        case .getPopularMoviesList:
+            return "/3/movie/popular"
+        case .getMovieDetails:
+            return "/3/movie/" + stringParameters
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .getPopularMoviesList, .getMovieDetails:
+            return "GET"
+        }
+    }
+}
+
+
--- 24iTest/Network/NetworkService.swift
+++ 24iTest/Network/NetworkService.swift
@@ -6,3 +6,134 @@
 //
 
 import Foundation
+
+enum NetworkResponse:String {
+    case success
+    case authenticationError = "Not found."
+    case badRequest = "Bad request"
+    case outdated = "The url you requested is outdated."
+    case failed = "Network request failed."
+    case noData = "Response returned with no data to decode."
+    case unableToDecode = "We could not decode the response."
+}
+
+enum Result<String>{
+    case success
+    case failure(String)
+}
+
+class NetworkService {
+    
+    class func request<T:Codable>(router: NetworkRouter, completion: @escaping(T) ->(), errorCompletion: @escaping(_ error: Error) -> ()) {
+        let session = URLSession(configuration: .default)
+        var components = URLComponents()
+        components.scheme = router.scheme
+        components.host = router.host
+        components.port = router.port
+        components.path = router.path
+        components.queryItems = router.parameters
+//        components.percentEncodedQuery = components.percentEncodedQuery?
+//            .replacingOccurrences(of: "+", with: "%2B")
+//            .replacingOccurrences(of: ":", with: "%3A")
+        
+        guard let url = components.url else {return}
+        let urlRequest = setupRequest(url: url, router: router)
+        let dataTask = session.dataTask(with: urlRequest) {data, response, error in
+            
+            if error != nil {
+                errorCompletion(error!)
+            }
+            if let response = response as? HTTPURLResponse {
+                let result = handleNetworkResponse(response)
+                switch result {
+                case .success:
+                    guard let responseData = data else {
+                        let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: NetworkResponse.noData.rawValue])
+                        errorCompletion(error)
+                        return
+                    }
+                    do {
+                        let responseObject = try JSONDecoder().decode(T.self, from: responseData)
+                        DispatchQueue.main.async {
+                            completion(responseObject)
+                        }
+                    }catch {
+                        let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: NetworkResponse.unableToDecode.rawValue])
+                        errorCompletion(error)
+                    }
+                case .failure(let networkFailureError):
+                    let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: networkFailureError])
+                    errorCompletion(error)
+                }
+            }
+            
+        }
+        dataTask.resume()
+    }
+    //when we expect jsonArray
+    class func request<T:Codable>(router: NetworkRouter, completion: @escaping([T]) ->(), errorCompletion: @escaping(_ error: Error) -> ()) {
+        let session = URLSession(configuration: .default)
+        var components = URLComponents()
+        components.scheme = router.scheme
+        components.host = router.host
+        components.port = router.port
+        components.path = router.path
+//        components.queryItems = router.parameters
+//        components.percentEncodedQuery = components.percentEncodedQuery?
+//            .replacingOccurrences(of: "+", with: "%2B")
+//            .replacingOccurrences(of: ":", with: "%3A")
+    
+        guard let url = components.url else {return}
+        let urlRequest = setupRequest(url: url, router: router)
+        let dataTask = session.dataTask(with: urlRequest) {data, response, error in
+            
+            if error != nil {
+                errorCompletion(error!)
+            }
+            if let response = response as? HTTPURLResponse {
+                let result = handleNetworkResponse(response)
+                switch result {
+                case .success:
+                    guard let responseData = data else {
+                        let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: NetworkResponse.noData.rawValue])
+                        errorCompletion(error)
+                        return
+                    }
+                    do {
+                        let responseObject = try JSONDecoder().decode([T].self, from: responseData)
+                        DispatchQueue.main.async {
+                            completion(responseObject)
+                        }
+                    }catch {
+                        let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: NetworkResponse.unableToDecode.rawValue])
+                        errorCompletion(error)
+                    }
+                case .failure(let networkFailureError):
+                    let error = NSError(domain:"", code:response.statusCode, userInfo:[ NSLocalizedDescriptionKey: networkFailureError])
+                    errorCompletion(error)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    class func setupRequest(url:URL,router:NetworkRouter) ->(URLRequest) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = router.method
+        //urlRequest.httpBody = router.httpBody
+     //   urlRequest.setValue(router.accessToken, forHTTPHeaderField: "authorization")
+        urlRequest.setValue("application/json", forHTTPHeaderField: "content-type")
+        return urlRequest
+    }
+    
+    class func handleNetworkResponse(_ response: HTTPURLResponse) ->  Result<String>{
+        switch response.statusCode {
+        case 200...299: return .success
+        case 401...500: return .failure(NetworkResponse.authenticationError.rawValue)
+        case 501...599: return .failure(NetworkResponse.badRequest.rawValue)
+        case 600: return .failure(NetworkResponse.outdated.rawValue)
+        default: return .failure(NetworkResponse.failed.rawValue)
+        }
+    }
+    
+}
